76f9a914e123d81910cd9403516c22d153de5f90
govldbstj <chldbstj0512@ajou.ac.kr>
2022-09-21 19:56:41 +0900

    latest

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-import { View,Text,StyleSheet,TextInput,Dimensions } from 'react-native';
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import { View,Text,StyleSheet,TextInput,FlatList, StatusBar, SafeAreaView } from 'react-native';
 import {DOMParser} from 'xmldom';
-import styled from 'styled-components/native';
+import ReactDOM, { render } from 'react-dom';
 
 function App() {
+
   const [station, setStation] = useState('');
   const [result, setResult] = useState([]);
-  const width=Dimensions.get('window').width;
+  //함수형 컴포넌트 const -> useEffect로 해결
 
- const printList = async()=>{
-  try{
-      <View style={styles.container}>
-        {console.log(result)}
-      {result.map(item => {
-          <Container>
-            <Content_name>{item.name}</Content_name>
-            <Content_locate>위치: {item.x}, {item.y}</Content_locate>
-        </Container>
-      }
-      )
-    }
-      </View>
+  const handleStation = text => {
+    setStation(text);
   }
-  catch(err){
-    alert(err)
-  }
-};
-
 
   const searchStation = async()=>{
-      try{
+    console.log("working");
+        try{
         var xhr = new XMLHttpRequest();
         var url = 'http://apis.data.go.kr/6410000/busstationservice/getBusStationList'; /*URL*/
         var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'UkgvlYP2LDE6M%2Blz55Fb0XVdmswp%2Fh8uAUZEzUbby3OYNo80KGGV1wtqyFG5IY0uwwF0LtSDR%2FIwPGVRJCnPyw%3D%3D'; /*Service Key*/
@@ -41,28 +27,34 @@ function App() {
             let xmlParser = new DOMParser();
             let xmlDoc = xmlParser.parseFromString(this.responseText, "text/xml");
             let i = 0;
+            let array = [];
             while(1){
               var tmpnode = new Object();
-              
-              console.log(i)
+              tmpnode.index = i;
               tmpnode.id = xmlDoc.getElementsByTagName("stationId")[i].textContent;
               tmpnode.name = xmlDoc.getElementsByTagName("stationName")[i].textContent;
               tmpnode.x = xmlDoc.getElementsByTagName("x")[i].textContent;
               tmpnode.y = xmlDoc.getElementsByTagName("y")[i].textContent;
-
-             result.push(tmpnode);
-              setResult(result);
+              array.push(tmpnode);
               i++;
               if(xmlDoc.getElementsByTagName("stationId")[i]==undefined) break;
             }
+            setResult(array);
           }
-        }
+        } 
       xhr.send();
       }
       catch(err){
         alert(err);
       }
+      if(result.length == 0){
+        console.log("result is empty");
+      }
   };
+
+  useEffect(()=>{
+    searchStation();
+  }, []);
 
   return(
     <View style={styles.container}>
@@ -70,14 +62,19 @@ function App() {
       <TextInput
      style={styles.input}
      placeholder='정류장 이름을 입력하세요'
-     value={station}
-     onChangeText={(text) => setStation(text)} 
-     onSubmitEditing={searchStation}
+     autoCorrect = {false}
+     value = {station}
+     onChangeText={handleStation}
+     onSubmitEditing = {()=>searchStation()}
+     multiline={false}
      returnKeyType="search"
       />
-      <View style={styles.container}>{console.log("this is", result)}</View>
-    </View>
+      <Text>{station}</Text>
+      <Text>{result.length}</Text>
+      <Text>{console.log(result)}</Text>
+      </View>
   );
+  
 }
 
 const styles=StyleSheet.create({
@@ -97,31 +94,9 @@ alignItems:'center'
 },
 title:{
   margin:10,
-  fontsize:30
+  fontsize:10
 }
 });
-const List = styled.ScrollView`
-flex: 1;
-width: ${({width})=> width - 40}px;
-`;
-const Container = styled.View`
-flex-direction: column;
-align-items: center;
-background-color: ${({ theme }) => theme.itemBackground};
-border-radius: 10px;
-padding: 5px;
-margin: 3px 0px;
-`;
-
-const Content_name = styled.Text`
-flex: 1;
-font-size: 20px;
-`;
-
-const Content_locate = styled.Text`
-flex: 1;
-font-size: 15px;
-`;
 
 
 export default App;
